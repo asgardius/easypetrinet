fc7f36767da84f9d1f8324569369c43a4e7bfd4e
Page Asgardius <asgardius@asgardius.company>
2021-03-25 12:33:16 -0700

    Setting some variables

--- src/easypetrinet/Easypetrinet.java
+++ src/easypetrinet/Easypetrinet.java
@@ -16,6 +16,11 @@ public class Easypetrinet {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int p1 = 1;
+        int p2 = 0;
+        int p3 = 1;
+        int p4 = 0;
+        int p5 = 1;
     }
     
 }
