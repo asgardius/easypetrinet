4483fa3d6f64c896fa3cdfbb6ef9421d13550ddb
Page Asgardius <asgardius@asgardius.company>
2021-03-25 13:24:36 -0700

    Setting some conditions

--- src/easypetrinet/Easypetrinet.java
+++ src/easypetrinet/Easypetrinet.java
@@ -21,11 +21,31 @@ public class Easypetrinet {
         int p3 = 1;
         int p4 = 0;
         int p5 = 1;
-        boolean t1 = true;
+        boolean t1 = false;
         boolean t2 = false;
-        boolean t3 = true;
-        boolean t4 = true;
+        boolean t3 = false;
+        boolean t4 = false;
         boolean t5 = false;
+        if (p1 >= 1 && p3 >= 1) {
+            t1 = true;
+            //System.out.println("t1");
+        }
+        if (p2 >= 1) {
+            t2 = true;
+            //System.out.println("t2");
+        }
+        if (p1 >= 1) {
+            t3 = true;
+            //System.out.println("t3");
+        }
+        if (p1 >= 1 && p5 >= 1) {
+            t4 = true;
+            //System.out.println("t4");
+        }
+        if (p4 >= 1) {
+            t5 = true;
+            //System.out.println("t5");
+        }
     }
     
 }
