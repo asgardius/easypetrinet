9609db8cd804a37eb5d8e9713a273382f8917bf7
Page Asgardius <asgardius@asgardius.company>
2021-03-26 13:42:53 -0700

    Replacing some variables

--- src/easypetrinet/Easypetrinet.java
+++ src/easypetrinet/Easypetrinet.java
@@ -16,35 +16,36 @@ public class Easypetrinet {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int p1 = 1;
-        int p2 = 0;
-        int p3 = 1;
-        int p4 = 0;
-        int p5 = 1;
+        //int p1 = 1;
+        //int p2 = 0;
+        //int p3 = 1;
+        //int p4 = 0;
+        //int p5 = 1;
+        int[] places = {1, 0, 1, 0 ,1};
         boolean t1 = false;
         boolean t2 = false;
         boolean t3 = false;
         boolean t4 = false;
         boolean t5 = false;
-        if (p1 >= 1 && p3 >= 1) {
+        if (places[0] >= 1 && places[2] >= 1) {
             t1 = true;
-            //System.out.println("t1");
+            System.out.println("t1");
         }
-        if (p2 >= 1) {
+        if (places[1] >= 1) {
             t2 = true;
-            //System.out.println("t2");
+            System.out.println("t2");
         }
-        if (p1 >= 1) {
+        if (places[0] >= 1) {
             t3 = true;
-            //System.out.println("t3");
+            System.out.println("t3");
         }
-        if (p1 >= 1 && p5 >= 1) {
+        if (places[0] >= 1 && places[4] >= 1) {
             t4 = true;
-            //System.out.println("t4");
+            System.out.println("t4");
         }
-        if (p4 >= 1) {
+        if (places[3] >= 1) {
             t5 = true;
-            //System.out.println("t5");
+            System.out.println("t5");
         }
     }
     
